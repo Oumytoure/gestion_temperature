e4e4438cffb772ae5b9cceb0e4f307f8f8741a55
oumytoure <toureoumy12@gmail.com>
2023-01-06 14:59:39 +0000

    teste

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { ConnexionComponent } from './connexion/connexion.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 
 
 
 
 const routes: Routes = [
- 
+  {path: "", pathMatch: "full", component: ConnexionComponent },
+  { path: 'inscrire', component: InscriptionComponent },
 ];
 
 @NgModule({
